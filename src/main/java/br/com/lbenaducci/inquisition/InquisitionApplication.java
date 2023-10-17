f726cb5738b7c35290eee898139114cce7c81247
Leo Benaducci <lbenaducci.dev@gmail.com>
2023-10-17 17:43:42 -0300

    Initial commit

---
+++ src/main/java/br/com/lbenaducci/inquisition/InquisitionApplication.java
@@ -0,0 +1,13 @@
+package br.com.lbenaducci.inquisition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquisitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquisitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lbenaducci/inquisition/InquisitionApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.lbenaducci.inquisition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class InquisitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
